fd97dbb4f698e4a9db0d53e61f642e5fdd325b5a
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-03-11 20:04:39 +0600

    Create 13148 - A Giveaway.cpp

---
+++ 13148 - A Giveaway.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define read freopen("input.txt", "r", stdin)
+#define write freopen("output.txt", "w", stdout)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define _for(i, j, k, in) for (int i = j; i < k; i += in)
+#define _rfor(i, j, k, in) for (int i = j; i >= k; i -= in)
+#define _foritaretor(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const double pi = acos(-1.0);
+
+vector<int>data{1, 64, 729, 4096, 15625, 46656, 117649, 262144, 531441,1000000, 1771561, 2985984, 4826809, 7529536, 11390625, 16777216, 24137569, 34012224,47045881, 64000000,85766121};
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	//read;write;
+  int n;
+  while (cin>>n&&n!=0)
+  {
+    if(binary_search(data.begin(),data.end(),n))cout<<"Special\n";
+    else cout<<"Ordinary\n";
+  }
+  
+    
+	return 0;
+}
